63f2570ace1c6c9949100c24cedc3bf7d148b22a
Alina Lavrik <alina_voro@mail.ru>
2022-10-13 21:54:46 +0300

    Add task 067_рекурсия_сумма цифр числа

---
+++ TASK_067/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9 */
+
+
+Console.WriteLine("Введите число N: ");
+int Num = int.Parse(Console.ReadLine());
+
+int SumOfDigits (int Num)
+{
+    if (Num < 10) return Num;
+    else return Num % 10 + SumOfDigits(Num / 10);
+}
+Console.WriteLine(SumOfDigits(Num));
+
