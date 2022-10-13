8422ea49e347628f20bf175c8a5fd9506f5ed9ea
Alina Lavrik <alina_voro@mail.ru>
2022-10-13 21:46:45 +0300

    add task 63_2_все натуральные числа в промежутке от M до N

---
+++ TASK_063/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+string Recurs (int m, int n)
+{
+    if (m == n) 
+    {
+        return n.ToString();
+    }
+
+    return (m + " " + Recurs(m + 1, n));   
+}
+Console.WriteLine(Recurs(m, n));
+
+
